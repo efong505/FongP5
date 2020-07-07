112e5e81ebef9b9c1e75f47384c9be7c7dba50f7
IVONNE NELSON <inelson1@cnm.edu>
2020-07-07 09:54:43 -0600

    Graded see todo comments

--- FongP5/Sprocket.cs
+++ FongP5/Sprocket.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//TODO:  Looks good   score 100/100
 namespace FongP5
 {
     /// <summary>
--- FongP5/SprocketOrderForm.xaml.cs
+++ FongP5/SprocketOrderForm.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace FongP5
 {
     /// <summary>
